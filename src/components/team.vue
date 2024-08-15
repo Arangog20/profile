b224d70f3c4b7419931e439aec8fe8a911915cda
Arangog20 <149792894+Arangog20@users.noreply.github.com>
2024-08-14 23:35:14 -0500

    creacion de team

---
+++ src/components/team.vue
@@ -0,0 +1,273 @@
+<template>
+    <div class="cards-container">
+  <div class="card">
+    <div class="card-header">
+      <div class="card-icon"><img src="https://www.freecodecamp.org/news/content/images/2022/02/image-76.png" alt="React Icon"></div>
+      <h3 class="card-title">React Developers</h3>
+    </div>
+    <p class="card-description">We don't make assumptions about the rest of your technology stack, so you can develop new features in React.</p>
+    <div class="card-footer">
+      
+      <div class="avatars">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona_23-2149436192.jpg?w=826&t=st=1723254812~exp=1723255412~hmac=a7fd36b7653e8325a5c3e82e6d11814f913d4a154f8fc5c821052aed723dd0e2" alt="Avatar 1">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona-cabello-rosado_23-2149436186.jpg?t=st=1723255343~exp=1723255943~hmac=74f2fd4c97abb996f007fcfa87b577d08342b88493d079feccc1f4c74df12258" alt="Avatar 2">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona-gafas-sol_23-2149436188.jpg?t=st=1723255512~exp=1723256112~hmac=b081d324b078fcbce54eb12e63c35bdf8f130aa683053c4fa150f8bd13dfba21" alt="Avatar 3">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona-gafas-sol_23-2149436180.jpg?t=st=1723322273~exp=1723322873~hmac=4725ba2d8db00355d73f9a913d508216f040938e7923d64e9ff73316ac65809e" alt="Avatar 4">
+      </div>
+      <div class="tags">
+        <span class="tag">React</span>
+        <span class="tag">MUI</span>
+      </div>
+    </div>
+  </div>
+
+  <div class="card">
+    <div class="card-header">
+      <div class="card-icon"><img src="https://w7.pngwing.com/pngs/980/549/png-transparent-vuejs-original-logo-icon.png" alt="Vue Icon"></div>
+      <h3 class="card-title">Vue.js Dev Team</h3>
+    </div>
+    <p class="card-description">The development of Vue and its ecosystem is guided by an international team, some of whom have chosen to be featured below.</p>
+    <div class="card-footer">
+      
+      <div class="avatars">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona_23-2149436192.jpg?w=826&t=st=1723254812~exp=1723255412~hmac=a7fd36b7653e8325a5c3e82e6d11814f913d4a154f8fc5c821052aed723dd0e2" alt="Avatar 1">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona-gafas_23-2149436185.jpg?t=st=1723255233~exp=1723255833~hmac=8cfe80a29a689e8a8ca918e1f13f8f1dad559dbec63df56354052deb09e2f5d6" alt="Avatar 2">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona-gafas-sol_23-2149436178.jpg?t=st=1723254812~exp=1723255412~hmac=fcbd12454bea8dc1179b8ec76637ce2c37e6861aa5df3653cc408f08c30a1223" alt="Avatar 3">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona-gafas-sol_23-2149436180.jpg?t=st=1723322273~exp=1723322873~hmac=4725ba2d8db00355d73f9a913d508216f040938e7923d64e9ff73316ac65809e" alt="Avatar 4">
+      </div>
+      <div class="tags">
+        <span class="tag">Vuejs</span>
+        <span class="tag">Developer</span>
+      </div>
+    </div>
+  </div>
+
+  <div class="card">
+    <div class="card-header">
+      <div class="card-icon"><img src="https://e7.pngegg.com/pngimages/64/743/png-clipart-adobe-xd-user-interface-design-computer-icons-adobe-systems-adobe-purple-violet.png" alt="Creative Icon"></div>
+      <h3 class="card-title">Creative Designers</h3>
+    </div>
+    <p class="card-description">A design or product team is more than just the people on it. A team includes the people, the roles they play, and the collaboration they foster.</p>
+    <div class="card-footer">
+      
+      <div class="avatars">
+        <img src="https://img.freepik.com/psd-gratis/ilustracion-3d-persona-gafas-sol-arco-iris_23-2149436181.jpg?t=st=1723254812~exp=1723255412~hmac=0db4379c74c6c693dbf5f9aa07d764057a8aaa2014934040b06919d40b8ce5ce" alt="Avatar 1">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona_23-2149436179.jpg?t=st=1723255233~exp=1723255833~hmac=1c128181abd723b211e35cf14cf63f29229afbbc9fa798da9821e1fe0d22df08" alt="Avatar 2">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona_23-2149436192.jpg?w=826&t=st=1723254812~exp=1723255412~hmac=a7fd36b7653e8325a5c3e82e6d11814f913d4a154f8fc5c821052aed723dd0e2" alt="Avatar 3">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona-calva_23-2149436183.jpg?t=st=1723322273~exp=1723322873~hmac=d59af65a1bd3e2b9ef020bafc05ee525959c473f26bc112dbab9335bf379f693" alt="Avatar 4">
+      </div>
+      <div class="tags">
+        <span class="tag">Sketch</span>
+        <span class="tag">XD</span>
+      </div>
+    </div>
+  </div>
+
+  <div class="card">
+    <div class="card-header">
+      <div class="card-icon"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu_QN5V4efHwltbVXrMfCV9oPnMPGhpy-xGHgaxaxDEs3d8ZuBUD6eyG213waFvFJvA5M&usqp=CAU" alt="Support Icon"></div>
+      <h3 class="card-title">Support Team</h3>
+    </div>
+    <p class="card-description">Support your team. Your customer support team is fielding the good, the bad, and the ugly day in and day out.</p>
+    <div class="card-footer">
+      
+      <div class="avatars">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona-gafas_23-2149436185.jpg?t=st=1723255233~exp=1723255833~hmac=8cfe80a29a689e8a8ca918e1f13f8f1dad559dbec63df56354052deb09e2f5d6" alt="Avatar 1">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona_23-2149436182.jpg?t=st=1723255512~exp=1723256112~hmac=33192f11560c028abd9be1dce877809e17b78db6cfe08d25e021d95279181c22" alt="Avatar 2">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona_23-2149436179.jpg?t=st=1723255233~exp=1723255833~hmac=1c128181abd723b211e35cf14cf63f29229afbbc9fa798da9821e1fe0d22df08" alt="Avatar 3">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona-calva_23-2149436183.jpg?t=st=1723322273~exp=1723322873~hmac=d59af65a1bd3e2b9ef020bafc05ee525959c473f26bc112dbab9335bf379f693" alt="Avatar 4">
+      </div>
+      <div class="tags">
+        <span class="tag">Zendesk</span>
+      </div>
+    </div>
+  </div>
+
+  <div class="card">
+    <div class="card-header">
+      <div class="card-icon"><img src="https://cdn-icons-png.flaticon.com/512/117/117965.png" alt="Marketing Icon"></div>
+      <h3 class="card-title">Digital Marketing</h3>
+    </div>
+    <p class="card-description">Digital marketing refers to advertising delivered through digital channels such as search engines, websites, and mobile apps.</p>
+    <div class="card-footer">
+      
+      <div class="avatars">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona-gafas-sol_23-2149436188.jpg?t=st=1723255512~exp=1723256112~hmac=b081d324b078fcbce54eb12e63c35bdf8f130aa683053c4fa150f8bd13dfba21" alt="Avatar 1">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona-gafas-sol_23-2149436178.jpg?t=st=1723254812~exp=1723255412~hmac=fcbd12454bea8dc1179b8ec76637ce2c37e6861aa5df3653cc408f08c30a1223" alt="Avatar 2">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona-cabello-rosado_23-2149436186.jpg?t=st=1723255343~exp=1723255943~hmac=74f2fd4c97abb996f007fcfa87b577d08342b88493d079feccc1f4c74df12258" alt="Avatar 3">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona-calva_23-2149436183.jpg?t=st=1723322273~exp=1723322873~hmac=d59af65a1bd3e2b9ef020bafc05ee525959c473f26bc112dbab9335bf379f693" alt="Avatar 4">
+      </div>
+      <div class="tags">
+        <span class="tag">Twitter</span>
+        <span class="tag">Email</span>
+      </div>
+    </div>
+  </div>
+
+  <div class="card">
+    <div class="card-header">
+      <div class="card-icon"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Google_Calendar_icon_%282020%29.svg/150px-Google_Calendar_icon_%282020%29.svg.png" alt="Event Icon"></div>
+      <h3 class="card-title">Event</h3>
+    </div>
+    <p class="card-description">Event is defined as a particular contest which is part of a program of contests. An example of an event is the long jump competition.</p>
+    <div class="card-footer">
+      
+      <div class="avatars">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona_23-2149436179.jpg?t=st=1723255233~exp=1723255833~hmac=1c128181abd723b211e35cf14cf63f29229afbbc9fa798da9821e1fe0d22df08" alt="Avatar 1">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona-cabello-rosado_23-2149436186.jpg?t=st=1723255343~exp=1723255943~hmac=74f2fd4c97abb996f007fcfa87b577d08342b88493d079feccc1f4c74df12258" alt="Avatar 2">
+        <img src="https://img.freepik.com/psd-gratis/ilustracion-3d-persona-pelo-punk-chaqueta_23-2149436198.jpg?t=st=1723255512~exp=1723256112~hmac=ad5910375650d0be51937c8d5263f1139b784059f9b81f87485ab5effeda70fe" alt="Avatar 3">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona-calva_23-2149436183.jpg?t=st=1723322273~exp=1723322873~hmac=d59af65a1bd3e2b9ef020bafc05ee525959c473f26bc112dbab9335bf379f693" alt="Avatar 4">
+      </div>
+      <div class="tags">
+        <span class="tag">Hublio</span>
+      </div>
+    </div>
+  </div>
+
+  <div class="card">
+    <div class="card-header">
+      <div class="card-icon"><img src="https://w7.pngwing.com/pngs/911/515/png-transparent-figma-logo-brand-logos-brands-in-colors-icon.png" alt="Figma Icon"></div>
+      <h3 class="card-title">Figma Resources</h3>
+    </div>
+    <p class="card-description">Explore, install, use, and remix thousands of plugins and files published to the Figma Community by designers and developers.</p>
+    <div class="card-footer">
+      
+      <div class="avatars">
+        <img src="https://img.freepik.com/psd-gratis/ilustracion-3d-persona-pelo-punk-chaqueta_23-2149436198.jpg?t=st=1723255512~exp=1723256112~hmac=ad5910375650d0be51937c8d5263f1139b784059f9b81f87485ab5effeda70fe" alt="Avatar 1">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona-gafas_23-2149436185.jpg?t=st=1723255233~exp=1723255833~hmac=8cfe80a29a689e8a8ca918e1f13f8f1dad559dbec63df56354052deb09e2f5d6" alt="Avatar 2">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona-gafas-sol_23-2149436188.jpg?t=st=1723255512~exp=1723256112~hmac=b081d324b078fcbce54eb12e63c35bdf8f130aa683053c4fa150f8bd13dfba21" alt="Avatar 3">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona-gafas-sol_23-2149436180.jpg?t=st=1723322273~exp=1723322873~hmac=4725ba2d8db00355d73f9a913d508216f040938e7923d64e9ff73316ac65809e" alt="Avatar 4">
+      </div>
+      <div class="tags">
+        <span class="tag">UI/UX</span>
+        <span class="tag">Figma</span>
+      </div>
+    </div>
+  </div>
+
+  <div class="card">
+    <div class="card-header">
+      <div class="card-icon"><img src="https://www.freecodecamp.org/news/content/images/2022/02/image-76.png" alt="Mobile Icon"></div>
+      <h3 class="card-title">Native Mobile App</h3>
+    </div>
+    <p class="card-description">React Native lets you create user friendly native apps and doesn’t compromise your users' experiences. whit its robust framework.</p>
+    <div class="card-footer">
+      
+      <div class="avatars">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona-gafas-sol_23-2149436178.jpg?t=st=1723254812~exp=1723255412~hmac=fcbd12454bea8dc1179b8ec76637ce2c37e6861aa5df3653cc408f08c30a1223" alt="Avatar 1">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona_23-2149436179.jpg?t=st=1723255233~exp=1723255833~hmac=1c128181abd723b211e35cf14cf63f29229afbbc9fa798da9821e1fe0d22df08" alt="Avatar 2">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona_23-2149436182.jpg?t=st=1723255512~exp=1723256112~hmac=33192f11560c028abd9be1dce877809e17b78db6cfe08d25e021d95279181c22" alt="Avatar 3">
+        <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona-gafas-sol_23-2149436180.jpg?t=st=1723322273~exp=1723322873~hmac=4725ba2d8db00355d73f9a913d508216f040938e7923d64e9ff73316ac65809e" alt="Avatar 4">
+      </div>
+      <div class="tags">
+        <span class="tag">React</span>
+      </div>
+    </div>
+  </div>
+
+  <div class="card">
+    <div class="card-header">
+      <div class="card-icon"><img src="https://w7.pngwing.com/pngs/410/100/png-transparent-web-development-html-responsive-web-design-logo-javascript-html-angle-web-design-text.png" alt="Beginners Icon"></div>
+      <h3 class="card-title">Only Beginners</h3>
+    </div>
+    <p class="card-description">Learn the basics of how websites work, front-end vs back-end, and using a code editor. Learn basic HTML, CSS and...</p>
+    <div class="card-footer">
+        <div class="avatars">
+            <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona-gafas_23-2149436185.jpg?t=st=1723255233~exp=1723255833~hmac=8cfe80a29a689e8a8ca918e1f13f8f1dad559dbec63df56354052deb09e2f5d6" alt="Avatar 1">
+            <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona-gafas-sol_23-2149436178.jpg?t=st=1723254812~exp=1723255412~hmac=fcbd12454bea8dc1179b8ec76637ce2c37e6861aa5df3653cc408f08c30a1223" alt="Avatar 2">
+            <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona-cabello-rosado_23-2149436186.jpg?t=st=1723255343~exp=1723255943~hmac=74f2fd4c97abb996f007fcfa87b577d08342b88493d079feccc1f4c74df12258" alt="Avatar 3">
+            <img src="https://img.freepik.com/psd-gratis/3d-ilustracion-persona-calva_23-2149436183.jpg?t=st=1723322273~exp=1723322873~hmac=d59af65a1bd3e2b9ef020bafc05ee525959c473f26bc112dbab9335bf379f693" alt="Avatar 4">
+      </div>
+      <div class="tags">
+        <span class="tag">CSS</span>
+        <span class="tag">HTML</span>
+      </div>
+      
+    </div>
+  </div>
+</div>
+
+    
+</template>
+
+<style lang="scss">
+
+.cards-container {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 25px;
+  padding-top: 20px;
+}
+
+.card {
+  background-color: #fff;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  .card-header {
+    
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+
+    .card-icon {
+      width: 40px;
+      height: 40px;
+      margin-right: 10px;
+    }
+}
+
+      img {
+       
+        width: 100%;
+        height:100%;
+        border-radius: 50%;
+      }
+    
+      .card-title {
+      font-size: 16px;
+      font-weight: 600;
+      margin: 0;
+    }
+  }
+
+  .card-description {
+    font-size: 14px;
+    color: #666;
+    margin: 10px 0;
+  }
+
+  .card-footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .tags {
+      .tag {
+        background-color: #f0f0f0;
+        border-radius: 4px;
+        padding: 4px 8px;
+        font-size: 12px;
+        margin-right: 5px;
+      }
+    }
+
+    .avatars {
+      display: flex;
+
+      img {
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        margin-left: -10px;
+        border: 2px solid #fff;
+      }
+    }
+    
+  }
+
+
+</style>
